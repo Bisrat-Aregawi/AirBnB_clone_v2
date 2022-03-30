a8abe3ddb390364bc91f21bd2a649aa813f73c30
Bisrat-Aregawi <bisrat_aregawi1914@yahoo.com>
2022-03-30 23:51:15 +0300

    Add PY script that starts a Flask web app

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Flask app"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def greeting():
+    """return string"""
+    return 'Hello HBNB!'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
