092fa39cdf5bdf4773050746d8b749e6dbdee72a
Bisrat-Aregawi <bisrat_aregawi1914@yahoo.com>
2022-04-01 11:19:36 +0300

    Add flask script to expose `/python/<text>` as well
    `text` has default value `is cool`

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Start Flask App"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def greetingHBNB():
+    """return greet message"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """return `HBNB` string"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def cMessage(text=None):
+    """return `C` followed by text"""
+    return 'C ' + text.replace('_', ' ')
+
+
+@app.route('/python/', defaults={'text': 'is cool'})
+@app.route('/python/<text>/', strict_slashes=False)
+def pythonMessage(text):
+    """return `python` followed by text"""
+    return 'Python ' + text.replace('_', ' ')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
