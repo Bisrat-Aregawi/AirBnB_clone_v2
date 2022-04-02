a6fe6304dfc21ce4a5bf90d2eecc05d202105f3e
Bisrat-Aregawi <bisrat_aregawi1914@yahoo.com>
2022-04-02 23:20:58 +0300

    Render all states on query to /states_list

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Start a Flask web application"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def statesList():
+    """Display unordered list of states"""
+    states = sorted(
+        list(storage.all(State).values()),
+        key=lambda x: x.name
+    )
+    return render_template('7-states_list.html', states=states)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """Close current storage session"""
+    storage.close()
+    return None
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
